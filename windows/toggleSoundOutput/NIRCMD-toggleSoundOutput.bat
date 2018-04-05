ffc7f7f3b8a44d86c83cbb477bc527662adebf3b
Schn33W0lf <37514596+Schn33W0lf@users.noreply.github.com>
2018-04-05 17:08:00 +0200

    Create NIRCMD-toggleSoundOutput.bat

---
+++ windows/toggleSoundOutput/NIRCMD-toggleSoundOutput.bat
@@ -0,0 +1,17 @@
+::@echo off
+:::::::::::::::::::::::::::::::::::::SETTINGS::::::::::::::
+set OUTPUT1=USB-Headset
+set OUTPUT2=Klinke-Lautsprecher
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+find /c /i "%OUTPUT1%" "defaultsounddevice.cookie"
+if errorlevel 1 goto false
+echo %OUTPUT2%>defaultsounddevice.cookie
+nircmd.exe setdefaultsounddevice %OUTPUT2%
+::DEBUG::pause
+GOTO exit
+:false
+echo %OUTPUT1%>defaultsounddevice.cookie
+nircmd.exe setdefaultsounddevice %OUTPUT1%
+::DEBUG::pause
+:exit
+::DEV::pause
