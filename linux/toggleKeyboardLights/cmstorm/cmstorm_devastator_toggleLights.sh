86662dbadfe01a2ed4610f44451ab98bab8ac080
Schn33W0lf <37514596+Schn33W0lf@users.noreply.github.com>
2020-04-25 11:36:21 +0200

    [FIX] didn't work under ubuntu 18.04.4
    $FLAGS were not 00000000 but e.g. 0000002
    (ffffe7f? -> scroll lock on, i.e. background lighting on
    ? can be c (no other leds on), d (caps on), e (numlock on) or f (caps & numlock on))
    (0000000? -> scroll lock off, i.e. background lighting off
    ? can be 0 (no leds on), 1 (caps on), 2 (numlock on) or 3 (caps & numlock on))
    => 0 = c; 1 = d (because c+1=d), 2 = e, 3 = f

--- linux/toggleKeyboardLights/cmstorm/cmstorm_devastator_toggleLights.sh
+++ linux/toggleKeyboardLights/cmstorm/cmstorm_devastator_toggleLights.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-FLAGS=$(xset -q | awk 'NR==2' | awk '{ print $10 }')
-if [ "$FLAGS" = 00000000 ]; then
-xset led on
+FLAGS=$(xset q | awk 'NR==2' | awk '{ print $10 }')
+if [[ ${FLAGS:0:1} == "0" ]]; then
+        xset led on
 else
-xset led off
+        xset led off
 fi
+
